1bebbd6d09697fce4c0453ccc5acab0a10dddfb0
sujilnt <sujilnt@gmail.com>
2019-03-27 16:10:41 +0000

    Updates

--- src/store/constant.js
+++
@@ -1,9 +0,0 @@
-// A file where all the constant is saved .
-export default {
-	INC: "INCREMENT",
-	DEC: "DECREMENT",
-	RESET:"RESET",
-	INC_SAGA: "INCREMENT_SAGA",
-	DEC_SAGA: "DECREMENT_SAGA",
-	RESET_SAGA:"RESET_SAGA"
-};
